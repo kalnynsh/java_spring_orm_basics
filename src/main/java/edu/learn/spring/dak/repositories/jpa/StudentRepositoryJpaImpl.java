5b0913a043b92c225495f0d31fe074ec0d72b7c6
= <=>
2020-09-12 18:11:15 +0300

    Updated StudentRepositoryJpaImpl and V2.

--- src/main/java/edu/learn/spring/dak/repositories/jpa/StudentRepositoryJpaImpl.java
+++ src/main/java/edu/learn/spring/dak/repositories/jpa/StudentRepositoryJpaImpl.java
@@ -1,21 +1,19 @@
 package edu.learn.spring.dak.repositories.jpa;
 
-import java.util.List;
-import java.util.Optional;
+import edu.learn.spring.dak.models.jpa.Student;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
-import org.springframework.stereotype.Repository;
-
-import edu.learn.spring.dak.models.jpa.Student;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class StudentRepositoryJpaImpl implements StudentRepositoryJpa {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@Override
 	public Optional<Student> findById(long id) {
 		return Optional.ofNullable(em.find(Student.class, id));
@@ -27,15 +25,15 @@ public class StudentRepositoryJpaImpl implements StudentRepositoryJpa {
 				.createQuery("select s from Student s", Student.class)
 				.getResultList();
 	}
-	
+
 	@Override
 	public Student save(Student student) {
 		if (student.getId() <= 0) {
 			em.persist(student);
-			
+
 			return student;
 		}
-		
+
 		return em.merge(student);
 	}
 }
--- src/main/java/edu/learn/spring/dak/repositories/jpa/StudentV2RepositoryJpaImpl.java
+++ src/main/java/edu/learn/spring/dak/repositories/jpa/StudentV2RepositoryJpaImpl.java
@@ -1,28 +1,26 @@
 package edu.learn.spring.dak.repositories.jpa;
 
-import java.util.List;
+import edu.learn.spring.dak.models.jpa.StudentV2;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-
-import org.springframework.stereotype.Repository;
-
-import edu.learn.spring.dak.models.jpa.StudentV2;
+import java.util.List;
 
 @Repository
 public class StudentV2RepositoryJpaImpl implements StudentV2RepositoryJpa {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	@Override
 	public List<StudentV2> findAllWithEntityGraph() {
 		EntityGraph<?> entityGraph = em.getEntityGraph("StudentWithAvatarAndEmails");
-		TypedQuery<StudentV2> query = em.createQuery("select s.* from StudentV2 s", StudentV2.class);
+		TypedQuery<StudentV2> query = em.createQuery("select s from StudentV2 s", StudentV2.class);
 		query.setHint("javax.persistence.fetchgraph", entityGraph);
-		
+
 		return query.getResultList();
 	}
 
